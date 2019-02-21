2858730a70b4ab5a2722f2e34a696670e7db7ac6
mcallisterd <47224703+mcallisterd@users.noreply.github.com>
2019-02-21 15:03:42 -0500

    Create  m_and_m.js

---
+++ m_and_m.js
@@ -0,0 +1,12 @@
+//Majors, Minors, and Programs of Study for Centre College
+
+//Set up D3 to be useable in browser
+var s = document.createElement("script");
+s.src= "https://d3js.org/d3.v5.js";
+var body = document.getElementsByTagName("body")[0];
+body.after(s);
+
+//Get all of the <a> elements from the page that are in the appropriate div
+var all_links = d3.selectAll("div.fusion-builder-row.fusion-row a").nodes();
+//And print them out
+all_links.forEach(function(e){console.log(e.innerText)});
